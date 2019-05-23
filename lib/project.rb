65520b453514889dfe31a87dc01d3a262a0716c4
Jason  Montes <jasone.montes@gmail.com>
2019-05-23 16:17:36 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -6,9 +6,10 @@ class Project
   def initialize(title)
     @title = title
     @backers = []
-    
+  end
+  
     def add_backer(backer)
     @backers << backer
     backer.backed_projects << self
   end
-  
+end
